a1f3637f7d74dfc1f3cc010da941a47bef51cd41
David.Handl <David.Handl@gar-sv-etp01.garching.physik.uni-muenchen.de>
2017-05-05 13:23:25 +0200

    included dataLoader in training

--- ML/config/samples.py
+++ ML/config/samples.py
@@ -1,15 +1,18 @@
 
-Preselection = "(dphi_jet0_ptmiss > 0.4) && (dphi_jet1_ptmiss > 0.4) && (n_jet>=4) && (n_bjet>0) && (jet_pt[0]>50e3) && (jet_pt[1]>25e3) && (jet_pt[2]>25e3) && (jet_pt[3]>25e3) && (mt>120e3) && !((mT2tauLooseTau_GeV > -0.5) && (mT2tauLooseTau_GeV < 80)) && (met >120e3)"
+Preselection = "(dphi_jet0_ptmiss > 0.4) && (dphi_jet1_ptmiss > 0.4) && (n_jet>=4) && (n_bjet>0) && (jet_pt[0]>50e3) && (jet_pt[1]>25e3) && (jet_pt[2]>25e3) && (jet_pt[3]>25e3) && (mt>130e3) && !((mT2tauLooseTau_GeV > -0.5) && (mT2tauLooseTau_GeV < 80)) && (met >230e3)"
 
 
-PREFIX = "/lustre/boerner/swup/stop1l-xaod/export/default_moriond17"
+PREFIX = "/project/etp5/dhandl/samples/SUSY/Stop1L/"
 Signal = [
-	Sample("stop_tN_250_62", load_chain([PREFIX+"/stop_tN_250_62/*.root"], "stop_tN_250_62_Nom"), ),
+	Sample("stop_bWN_350_200", load_chain([PREFIX+"/stop_bWN_350_200/*.root"], "stop_bWN_350_200_Nom"), ),
 	]
 
 Backgrounds = [
-	Sample("ttbar", load_chain([PREFIX+"/powheg_ttbar/*.root"], "powheg_ttbar_Nom"), ),
-	Sample("Wjets", load_chain([PREFIX+"/sherpa22_Wjets/*.root"], "sherpa22_Wjets_Nom"), ),
+	Sample("ttbar",     load_chain([PREFIX+"/powheg_ttbar/*.root"],       "powheg_ttbar_Nom"), ),
+	Sample("Wjets",     load_chain([PREFIX+"/sherpa22_Wjets/*.root"],     "sherpa22_Wjets_Nom"), ),
+	Sample("singletop", load_chain([PREFIX+"/powheg_singletop/*.root"],   "powheg_singletop_Nom"), ),
+	Sample("Diboson",   load_chain([PREFIX+"/sherpa221_diboson/*.root"],  "sherpa221_diboson_Nom"), ),
+	Sample("ttV",       load_chain([PREFIX+"/amcnlo_ttV/*.root"],         "amcnlo_ttV_Nom"), ),
 ]
 
-Weight = "xs_weight * weight * sf_total"
+Weight = "xs_weight * weight * sf_total * weight_sherpa22_njets"
--- ML/config/variables.py
+++ ML/config/variables.py
@@ -1,17 +1,8 @@
 met
 mt
-m_top_chi2
-ht_sig
-dr_bjet_lep
-
-dphi_ttbar
-dphi_leptop_met
-dphi_hadtop_met
-
-n_jet $I
-jet_pt[2]
-jet_pt[3]
-ht
+n_bjet
+dphi_met_lep
+amt2
 
 ?sf_total
 ?xs_weight

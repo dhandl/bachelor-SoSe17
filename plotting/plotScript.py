675f7cccb0427dd6122540d621dc8e313f3ee923
Alessandro.Schwemmer <alessandro.schwemmer@physik.uni-muenchen.de>
2018-05-16 18:09:38 +0200

    minor changes

--- plotting/plotScript.py
+++ plotting/plotScript.py
@@ -305,7 +305,9 @@ def main():
     
     #cut = "(n_jet>=4) && (n_lep==1) && (n_bjet>=1) && (met>=200e3) && (mt>=90e3) && (dphi_jet0_ptmiss > 0.4) && (dphi_jet1_ptmiss > 0.4) && ( (dphi_b_lep_max<2.5) || (dphi_b_lep_max>2.5 && ((ht>300e3) || (ht<200e3)) ) ) && !((mT2tauLooseTau_GeV > -0.5) && (mT2tauLooseTau_GeV < 80))"
     
-    cut = "(n_jet>=4) && (n_lep==1) && (n_bjet>=1) && (met>=200e3) && (mt>=90e3) && (dphi_jet0_ptmiss > 0.4) && (dphi_jet1_ptmiss > 0.4) && !((mT2tauLooseTau_GeV > -0.5) && (mT2tauLooseTau_GeV < 80))"
+    #cut = "(n_jet>=4) && (n_lep==1) && (n_bjet>=1) && (met>=250e3) && (mt>=0e3) && (dphi_jet0_ptmiss > 0.4) && (dphi_jet1_ptmiss > 0.4) && !((mT2tauLooseTau_GeV > -0.5) && (mT2tauLooseTau_GeV < 80))"
+
+    cut = "(n_jet>=4) && (n_lep==1) && (n_bjet>=1) && (met>=250e3) && (mt>=0e3) && !((mT2tauLooseTau_GeV > -0.5) && (mT2tauLooseTau_GeV < 80))"
     
     weight = str(lumi)+" * weight * xs_weight * sf_total * weight_sherpa22_njets"
     
